bdd8b2fb861328b919b7163664a4ac201d2b6c23
shengqiwu <shengqiwu@shein.com>
2021-09-21 16:37:36 +0800

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>{{ data.count }}</h1>
+  <h1>{{ greetings }}</h1>
+  <h1>{{ x }}</h1>
+  <h1>{{ y }}</h1>
+  <button @click="updateGreeting">+++</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { computed, onMounted, onUnmounted, reactive, ref, watch } from "vue";
+import useMousePosition from "./hooks/useMousePosition";
+interface DataProps {
+  count: number;
+  increase: () => void;
+  double: number;
+}
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+export default {
+  name: "App",
+  setup() {
+    const data: DataProps = reactive({
+      count: 0,
+      increase: () => {
+        data.count++;
+      },
+      double: computed(() => data.count * 2),
+    });
+    const greetings = ref("");
+    const updateGreeting = () => {
+      greetings.value += "Hello!";
+    };
+    const { x, y } = useMousePosition();
+
+    watch(greetings, (newValue, oldValue) => {
+      document.title = greetings.value;
+    });
+    return {
+      data,
+      greetings,
+      updateGreeting,
+      x,
+      y,
+    };
+  },
+};
 </script>
 
 <style>
